3ca29f8126f0c82af054f940368f5926a01060d0
zhuangjc <976544643@qq.com>
2023-02-15 16:31:45 +0800

    添加是否执行促销活动。

--- src/main/java/com/service/service/Impl/StrawberryPayServiceImpl.java
+++ src/main/java/com/service/service/Impl/StrawberryPayServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.Scanner;
 
 /**
  * @author zjc
@@ -22,8 +23,19 @@ public class StrawberryPayServiceImpl implements FruitPayService, InitializingBe
             return null;
         }
 
-        //芒果价格  实际可以去数据库拿取
-        BigDecimal strawberryPrice = new BigDecimal("13");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("草莓是否以促销价出售 1、是  2、否");
+        int select = scanner.nextInt();
+        BigDecimal strawberryPrice = new BigDecimal("0");
+        if(select == 1) {
+            //芒果价格  实际可以去数据库拿取
+            strawberryPrice = new BigDecimal("13");
+            strawberryPrice = strawberryPrice.multiply(new BigDecimal("0.8"));
+        }else if (select == 2) {
+            //芒果价格  实际可以去数据库拿取
+             strawberryPrice = new BigDecimal("13");
+        }
+
         BigDecimal finalPrice = strawberryPrice.multiply(num);
         System.out.println("草莓价格="+finalPrice);
 
--- src/test/java/com/test.java
+++ src/test/java/com/test.java
@@ -15,7 +15,7 @@ public class test {
     @Test
     public void testPrice(){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("请选择水果种类：1、苹果  2、草莓  3、芒果  4、退出(为了体验，这里不做真的退出，只是清除数据)");
+        System.out.println("请选择水果种类：1、苹果  2、草莓  3、芒果  4、退出为了体验，这里不做真的退出，只是做清除数据操作,同时计算是否优惠后(满100-20)的价格))");
         BigDecimal zero = new BigDecimal("0");
         BigDecimal sumPrice = new BigDecimal("0");
         BigDecimal appleFinalPrice = new BigDecimal("0");
@@ -55,23 +55,58 @@ public class test {
                 int i = scanner.nextInt();
                 BigDecimal num = new BigDecimal(i);
                 strawberryFinalPrice = strawberry.quote(num);
-                sumPrice = sumPrice.add(strawberryFinalPrice);
-                System.out.println("sumPrice="+sumPrice);
+                //计算总价
+                try{
+                    sumPrice = sumPrice.add(strawberryFinalPrice);
+                    System.out.println("sumPrice="+sumPrice);
+                }catch (NullPointerException e) {
+                    System.out.println("最终价格返回为NULL，水果数量不能为负数");
+                }
+                catch(Exception e){
+                    System.out.println("运行出现异常，请联系管理员");
+                }finally {
+                    //System.out.println("运行出现异常，请联系管理员");
+                }
+
             } else if(input ==3) {
                 FruitPayService mango = PayServiceStrategyFactory.getFruitType("mango");
                 System.out.println("请输入购买芒果的数量");
                 int i = scanner.nextInt();
                 BigDecimal num = new BigDecimal(i);
                 mangoFinalPrice = mango.quote(num);
-                sumPrice = sumPrice.add(mangoFinalPrice);
-                System.out.println("sumPrice="+sumPrice);
+                //计算总价
+                try{
+                    sumPrice = sumPrice.add(mangoFinalPrice);
+                    System.out.println("sumPrice="+sumPrice);
+                }catch (NullPointerException e) {
+                    System.out.println("最终价格返回为NULL，水果数量不能为负数");
+                }
+                catch(Exception e){
+                    System.out.println("运行出现异常，请联系管理员");
+                }finally {
+                    //System.out.println("运行出现异常，请联系管理员");
+                }
             } else if(input ==4) {
+                System.out.println("是否执行满100优惠20  1、是  2、否");
+                int select  = scanner.nextInt();
+                if(select ==1){
+                    BigDecimal times = sumPrice.divide(new BigDecimal("100")).setScale(0,BigDecimal.ROUND_FLOOR);
+                    sumPrice=sumPrice.subtract(times.multiply(new BigDecimal("20")));
+                    System.out.println("最终价格=" + sumPrice);
+                }else if (select ==2){
+                    System.out.println("最终价格=" + sumPrice);
+                }
+
+                /*
+                * 清除数据
+                * */
                 appleFinalPrice.multiply(zero);
                 strawberryFinalPrice.multiply(zero);
                 mangoFinalPrice.multiply(zero);
+                sumPrice.multiply(zero);
                 //System.exit(0);
             }
-            System.out.println("请继续选择水果种类：1、苹果  2、草莓  3、芒果  4、退出(为了体验，这里不做真的退出，只是做清除数据操作)");
+            System.out.println("请继续选择水果种类：1、苹果  2、草莓  3、芒果  4、退出(为了体验，这里不做真的退出，只是做清除数据操作,同时计算是否优惠后(满100-20)的价格))");
 
         }
     }
